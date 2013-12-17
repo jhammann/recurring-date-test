4bd32ec616db7d7b133b569fe4913b96020bd4bb
Ham <jeroenhammann@gmail.com>
2013-12-17 11:32:06 +0100

    added compatibility for non-recurring events

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 I wanted to learn more about recurring dates so I created a small project with a simple UI ([Semantic UI](http://semantic-ui.com/)).
 I use the [ice_cube](https://github.com/seejohnrun/ice_cube) Rubygem for handling repeated events and I use the [recurring_select](https://github.com/GetJobber/recurring_select) gem for it's helpers and selectors.
+
+I added Dutch localization to recurring_select. The entire app is in Dutch too :)
--- app/models/dateslot.rb
+++ app/models/dateslot.rb
@@ -5,12 +5,20 @@ class Dateslot < ActiveRecord::Base
   serialize :schedule, Hash
 
   def schedule=(new_schedule)
-    write_attribute(:schedule, RecurringSelect.dirty_hash_to_rule(new_schedule).to_hash)
+    if RecurringSelect.is_valid_rule?(new_schedule)
+      write_attribute(:schedule, RecurringSelect.dirty_hash_to_rule(new_schedule).to_hash)
+    else
+      write_attribute(:schedule, nil)
+    end
   end
 
   def converted_schedule
-    the_schedule = Schedule.new(self.date)
-    the_schedule.add_recurrence_rule(RecurringSelect.dirty_hash_to_rule(self.schedule))
-    the_schedule
+    if RecurringSelect.is_valid_rule?(self.schedule)
+      the_schedule = Schedule.new(self.date)
+      the_schedule.add_recurrence_rule(RecurringSelect.dirty_hash_to_rule(self.schedule))
+      the_schedule
+    else
+      self.date
+    end
   end
 end
