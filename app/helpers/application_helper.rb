b7d8b67d10b34ddfb70363787a0009cb46f76564
Ham <jeroenhammann@gmail.com>
2014-01-21 17:20:54 +0100

    restored application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 module ApplicationHelper
 
+  def first_event(dateslot, schedule)
+    if RecurringSelect.is_valid_rule?(schedule)
+      l(dateslot.converted_schedule.first, format: :full)
+    else
+      l(dateslot.date, format: :full)
+    end
+  end
+
+  def valid_schedule?(schedule)
+    return true if RecurringSelect.is_valid_rule?(schedule)
+  end
+
 end
